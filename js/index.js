8a685b918c6d8d5be9946d32520b2e09da06d8fc
Lorie-Ann Ekpo <146092443+LorieEkpo@users.noreply.github.com>
2024-09-13 13:26:36 +0000

    Added js file for variable data + and json for adding data.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+let title = "";
+let welcomeText = "";
+let aboutHeader = "";
+let aboutText = "";
+let skillHeader = "";
+let skillText;
+let projectHeader;
+let projectText;
+
+const skills = [];
+
+//add fetch to get skills array and create skills array
+function skillList(){
+
+}
