1cc85619a9a2b08d82c30bfc9562277f4dea5796
Lorie-Ann Ekpo <146092443+LorieEkpo@users.noreply.github.com>
2024-09-19 16:00:03 +0000

    Added variables in index.js for use in index.html

--- js/index.js
+++ js/index.js
@@ -1,16 +1,53 @@
+//planning on adding json file for language options
+//currently using set values for each variable
 
-let title = "";
-let welcomeText = "";
-let aboutHeader = "";
-let aboutText = "";
-let skillHeader = "";
-let skillText;
-let projectHeader;
-let projectText;
+let navHome = "Hem";
+let navAbout = "Om mig";
+let navSkills = "Färdigheter";
+let navProjects = "Projekt";
+let title = "Mjukvaruutvecklare";
+let welcomeText = "Välkommen till mitt projektportfolio";
+let aboutHeader = "Om mig";
+let aboutText = "Här är en text om mig";
+let skillHeader = "Färdigheter";
+/* for future variable skill list
+let skillList = [
+        "Färdighet", 
+        "Färdighet",
+        "Färdighet", 
+        "Färdighet"
+    ];
+*/
+let projectHeader = "Projekt";
+let projectText = "Här är mina projekt";
 
-const skills = [];
+//displays variables as text
+function displayText() {
+    document.getElementById("navHome").innerHTML = navHome;
+    document.getElementById("navAbout").innerHTML = navAbout;
+    document.getElementById("navSkills").innerHTML = navSkills;
+    document.getElementById("navProjects").innerHTML = navProjects;
+    document.getElementById("title").innerHTML = title;
+    document.getElementById("welcomeText").innerHTML = welcomeText;
+    document.getElementById("aboutHeader").innerHTML = aboutHeader;
+    document.getElementById("aboutText").innerHTML = aboutText;
+    document.getElementById("skillHeader").innerHTML = skillHeader;
+    //document.getElementById("skills").innerHTML =
+    document.getElementById("projectHeader").innerHTML = projectHeader;
+    document.getElementById("projectText").innerHTML = projectText;
+}
+
+/*
+//function for choosing language
+
+function setLanguage(){
+    if (document.documentElement.lang === "en"){
+        //add code for english translate
+    }
+    else{
+        //add code for swedish language
+    }
+}
+*/
 
 //add fetch to get skills array and create skills array
-function skillList(){
-
-}
